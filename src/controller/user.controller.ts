ad7ab112a27c066645659620af0f7a6c5c84964a
guoyansai <guoyansai@qq.com>
2022-10-16 15:56:03 +0800

    处理路由分拆

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -2,6 +2,9 @@ class UserController {
   async register(ctx: { body: string }, next: any) {
     ctx.body = "用户注册成功";
   }
+  async login(ctx: { body: string }, next: any) {
+    ctx.body = "用户登录成功";
+  }
 }
 
 module.exports = new UserController();
--- src/router/user.route.ts
+++ src/router/user.route.ts
@@ -1,8 +1,10 @@
 const Router = require("koa-router");
-const { register } = require("../controller/user.controller");
+const { register, login } = require("../controller/user.controller");
 
 const router = new Router({ prefix: "/users" });
 // 注册接口
-router.post("/", register);
+router.post("/register", register);
+// 登录接口
+router.post("/login", login);
 
 module.exports = router;
